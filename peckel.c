fb1cc9178626408b66f1f8328ecfca9707f603ce
BeanLover69 <borissopko5@gmail.com>
2026-02-19 16:13:31 +0100

    fix of --help

--- peckel.c
+++ peckel.c
@@ -2,19 +2,19 @@
 #include <stdbool.h>
 #include <X11/Xlib.h>
 
-void option(char *str[]);
+void option(char *str);
 
-void option(char *str[])
+void option(char *str)
 {
-	char *happy[] = "--help";
-	if (*str == *happy) {
+	char happy = "--help";
+	if (str == happy) {
 		printf("Hello!\nThis Command is an Entertainment Tool\nand only things you can do with it is to type peckel -Emotion replace Emotion\nwith the Emotion wished for for the Character");
 	}
 }
 
 int main(int argc, char *argv[]) {
           if (*argv[0] == '-') {
-        	    option(*argv);
+        	    option(argv);
 	  }
   	  bool Running = true;
   	  XEvent event;
