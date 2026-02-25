b6493fcc50f776aea15fba4a141bedabacfd3fce
BeanLover69 <borissopko5@gmail.com>
2026-02-25 21:06:47 +0100

    trying to fix subcommands

--- peckel.c
+++ peckel.c
@@ -2,19 +2,19 @@
 #include <stdbool.h>
 #include <X11/Xlib.h>
 
-void option(char *str[]);
+void option(char *str);
 
-void option(char *str[])
+void option(char *str)
 {
-	char *happy[] = "--help";
+	char *happy = "--help";
 	if (str == happy) {
 		printf("Hello!\nThis Command is an Entertainment Tool\nand only things you can do with it is to type peckel -Emotion replace Emotion\nwith the Emotion wished for for the Character");
 		return 0;
 	}
 }
 
-int main(int argc, char *argv[]) {
-	option(argv);
+int main(int argc, char *argv) {
+	option(*argv);
   	bool Running = true;
   	XEvent event;
   	Display* dis = XOpenDisplay(NULL);
