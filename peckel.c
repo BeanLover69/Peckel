bd4304e9c4ca4be91eb79188579354da41911a4f
BeanLover69 <borissopko5@gmail.com>
2026-04-05 12:11:25 +0200

    --help fix

--- peckel.c
+++ peckel.c
@@ -5,7 +5,7 @@
 
 int option(char str[])
 {
-	if (strcmp(str, "--help") != 0) {
+	if (strcmp(str, "--help")) {
 		printf("Hello!\nThis Command is an Entertainment Tool\nand only things you can do with it is to type peckel -Emotion replace Emotion\nwith the Emotion wished for for the Character\n");
 		return 0;
 	}
