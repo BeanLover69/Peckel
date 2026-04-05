fc46c54b8c75f14ef4d8885795a0bc9db261abfd
BeanLover69 <borissopko5@gmail.com>
2026-04-05 12:58:54 +0200

    making program not run if help is wanted

--- peckel.c
+++ peckel.c
@@ -9,37 +9,40 @@ int option(char str[])
 		printf("Hello!\nThis Command is an Entertainment Tool\nand only things you can do with it is to type peckel -Emotion replace Emotion\nwith the Emotion wished for for the Character\n");
 		return 0;
 	}
-	return 0;
+	else {
+	  	bool Running = true;
+  		XEvent event;
+  		Display* dis = XOpenDisplay(NULL);
+  		Atom delete = XInternAtom(dis, "WM_DELETE_WINDOW", False);
+  		Window w = XCreateSimpleWindow(dis, XDefaultRootWindow(dis),100,100,100,100, 1, WhitePixel(dis, 0), BlackPixel(dis, 0));
+  		XMapWindow(dis, w);
+  		unsigned long col = (100,100,100,100);
+  		XSetForeground(dis, DefaultGC(dis, 0), WhitePixel(dis, 0));
+  		XSelectInput(dis, w, ExposureMask);
+  		while(Running) {
+  		        if (event.type == ClientMessage && event.xclient.data.l[0] == delete)
+  		        {
+  		        	Running = false;
+  		        	XDestroyWindow(dis, w);
+		        	return 0;
+		        }
+        		    
+        		XNextEvent(dis, &event);
+        		if(event.type == Expose) {
+			        XSetForeground(dis, DefaultGC(dis, 0), WhitePixel(dis, 0));
+				XFillRectangle(dis,w,DefaultGC(dis, 0), 10, 10, 100, 100);
+				XSetForeground(dis,DefaultGC(dis, 0), col);
+				XFillRectangle(dis,w,DefaultGC(dis, 0), 100,100, 190, 190);
+        		}
+        	}
+		return 0;
+		
+		}
+		return 0;
 }
 
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
 		option(argv[1]);
 	}
-  	bool Running = true;
-  	XEvent event;
-  	Display* dis = XOpenDisplay(NULL);
-  	Atom delete = XInternAtom(dis, "WM_DELETE_WINDOW", False);
-  	Window w = XCreateSimpleWindow(dis, XDefaultRootWindow(dis),100,100,100,100, 1, WhitePixel(dis, 0), BlackPixel(dis, 0));
-  	XMapWindow(dis, w);
-  	unsigned long col = (100,100,100,100);
-  	XSetForeground(dis, DefaultGC(dis, 0), WhitePixel(dis, 0));
-  	XSelectInput(dis, w, ExposureMask);
-  	while(Running) {
-  	        if (event.type == ClientMessage && event.xclient.data.l[0] == delete)
-  	        {
-  	        	Running = false;
-  	        	XDestroyWindow(dis, w);
-	        	return 0;
-	        }
-        	    
-        	XNextEvent(dis, &event);
-        	if(event.type == Expose) {
-		        XSetForeground(dis, DefaultGC(dis, 0), WhitePixel(dis, 0));
-			XFillRectangle(dis,w,DefaultGC(dis, 0), 10, 10, 100, 100);
-			XSetForeground(dis,DefaultGC(dis, 0), col);
-			XFillRectangle(dis,w,DefaultGC(dis, 0), 100,100, 190, 190);
-        	}
-        }
-	return 0;
 }
